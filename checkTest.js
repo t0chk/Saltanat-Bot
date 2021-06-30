014a5b78eb1216b39029cb9f941866fb85e825b8
t0chk <86585817+t0chk@users.noreply.github.com>
2021-06-30 09:30:13 +0300

    Update checkTest.js

--- checkTest.js
+++ checkTest.js
@@ -1,12 +1,25 @@
+function updateVolume(){
+    var response = UrlFetchApp.fetch("https://fapi.binance.com/fapi/v1/ticker/24hr");
+    let data = JSON.parse(response.getContentText());
+    b=2
+    for (d in data){
+      vol24sheet.getRange("A"+b).setValue(data[d].symbol)
+      vol24sheet.getRange("B"+b).setValue(data[d].quoteVolume)
+      b++
+    }
+
+}
+
 function buttonCheckcom() {
 
-  var message = com.getRange("B2").getValue();
-
-  comCommands.message = message;
-  comCommands.comCheck();
-  message = comCommands.message;
-
-  obrabotkaMesageClass(message)
+  let  message = com.getRange("B2").getValue();
+  let status=settingSheet.getRange("B5").getValue();
+  if (status=="выкл"){
+    obrabotkaMesageClass(message)
+  }
+  else if (status=="вкл"){
+    obrabotkaMesageClassTurbo(message)
+  }
 }
 
 // функция для обновления фильтров 
@@ -47,4 +60,35 @@ function updateFilter() {
   // done 
   filterSheet.getRange("F2").setValue("done");
 }
+
+
+function BswapSend(){
+  binance.BinanceBSwap()
+  let res =binance.binanceConnect()
+  let count = swapSheet.getLastRow()
+  swapSheet.getRange("A"+(count+1)).setValue(res[0].share.asset.EUR)
+  swapSheet.getRange("B"+(count+1)).setValue(res[0].share.asset.BUSD)
+  
+  teleg.text="eur="+res[0].share.asset.EUR+" \n"+"busd="+res[0].share.asset.BUSD
+  teleg.telegramSendText()
+}
+
+function clearLogi(){
+  let count = logiSheet.getLastRow()
+  for (let i = 2; i<=count;i++){
+    logiSheet.getRange("B"+i).setValue("")
+    logiSheet.getRange("C"+i).setValue("")
+  }
+    logiSheet.getRange("A1").setValue(0)
+
+}
+
+function testtrig(s){
+   let maxantiban=Number(settingSheet.getRange("B10").getValue())
+   console.log(maxantiban)
+}
+
+
+
+
 
