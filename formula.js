859197686ae71ff8d4c80b3efc397e4d29231f98
t0chk <86585817+t0chk@users.noreply.github.com>
2021-06-30 09:31:37 +0300

    Update formula.js

--- formula.js
+++ formula.js
@@ -1,24 +1,21 @@
-class formula{
-    constructor(){
-        this.status='false'
-        this.answerSheet='0'
-        this.rowId=undefined
-        this.rowText=undefined
+class logi{
+    constructor() {
+        this.messageTradingview = "";
+        this.messageAnswerExchange = "";
+        this.countLog = 0;
+        this.statusLog = false;
     }
-    checkFormulaStatus(){
-        this.answerSheet=formulaSheet.getRange("A1").getValue()
-        this.status=false;
-        if ((formul.answerSheet == 1) || (formul.answerSheet == "1")){
-        	this.status=true;
-        }
-    }
-    rowWtire(){
-        if (this.rowId!=undefined){
-            formulaSheet.getRange(this.rowId).setValue(this.rowText)
-            this.rowId=''
-            this.rowText=''
+    ///добавления логов
+    addLog() {
+        if (this.statusLog=="вкл") {
+            this.countLog=logiSheet.getRange("A1").getValue();
+            logiSheet.getRange("C" + Number(this.countLog + 2)).setValue(this.messageTradingview);
+            logiSheet.getRange("B" + Number(this.countLog + 2)).setValue(this.messageAnswerExchange);
+            this.countLog += 1;
+            logiSheet.getRange("A1").setValue(Number(this.countLog));
         }
     }
 }
-///////////formula
-var formul = new formula();
+//////log
+var loggi = new logi();
+loggi.statusLog=settingSheet.getRange("B1").getValue();
